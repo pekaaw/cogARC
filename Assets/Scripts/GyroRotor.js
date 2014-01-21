cd8a51145fc6800f07879c49fb8ea74aa713fc63
Jakob Sand Svarstad <watashiwaore@gmail.com>
2014-01-21 15:23:36 +0100

    , i am currently working on the gyro in scene "Game1.unity". I got the gyro working, just incorrectly. I have to fix the axises and do calibrations and find out how we are going to connect the world to the gyro.

--- Assets/Scripts/GyroRotor.js
+++ Assets/Scripts/GyroRotor.js
@@ -1,9 +1,36 @@
 ﻿#pragma strict
-
 function Start () {
-
+if(SystemInfo.supportsGyroscope){
+	Screen.orientation = ScreenOrientation.Portrait;
+	Input.gyro.enabled = true;
+	}
 }
 
-function Update () {
 
+function Update () {
+	if(Input.gyro.enabled){
+	
+		gameObject.transform.rotation = ConvertRotation(Input.gyro.attitude) * GetRotFix();
+		var a : String = Input.gyro.attitude.ToString();
+		gameObject.GetComponentInChildren(TextMesh).text = a;
+	}
+}
+
+private static function ConvertRotation(q : Quaternion) : Quaternion
+{
+    return new Quaternion(q.x, q.y, -q.z, -q.w);
+}
+
+private function GetRotFix() : Quaternion
+{
+    if (Screen.orientation == ScreenOrientation.Portrait)
+        return Quaternion.identity;
+    if (Screen.orientation == ScreenOrientation.LandscapeLeft
+    || Screen.orientation == ScreenOrientation.Landscape)
+        return Quaternion.Euler(0, 0, -90);
+    if (Screen.orientation == ScreenOrientation.LandscapeRight)
+        return Quaternion.Euler(0, 0, 90);
+    if (Screen.orientation == ScreenOrientation.PortraitUpsideDown)
+        return Quaternion.Euler(0, 0, 180);
+    return Quaternion.identity;
 }
