872c37d9e1cf111ebc6da3fba5f2554630943ecc
Jakob Sand Svarstad <watashiwaore@gmail.com>
2014-01-31 12:32:32 +0100

    got the gyro working nicely. it reports which side the colision was on. i have not yet separated top and bottom.

--- Assets/Scripts/GyroRotor.js
+++ Assets/Scripts/GyroRotor.js
@@ -15,7 +15,6 @@ function Start () {
 if(SystemInfo.supportsGyroscope){
 	//Screen.orientation = ScreenOrientation.PortraitUpsideDown;
 	Input.gyro.enabled = true;
-	//Input.compass.enabled = true;
 	}
 }
 
@@ -28,6 +27,19 @@ function Update () {
 		gameObject.transform.rotation.z = -newRot.z;
 		gameObject.transform.rotation.w = newRot.w;
 		*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 		newRot = Quaternion.Inverse(newRot);
 		
@@ -44,18 +56,34 @@ function Update () {
 		
 	//	}*/
 	
-		var topVec3 : UnityEngine.Vector3 = gameObject.FindGameObjectWithTag("MyWorldVectorY").transform.position;
-		var centerVec3 : UnityEngine.Vector3 = gameObject.FindGameObjectWithTag("MyWorldVectorCenter").transform.position;
+		var topVec3 : UnityEngine.Vector3 = GameObject.FindGameObjectWithTag("MyWorldVectorY").transform.position;
+		var centerVec3 : UnityEngine.Vector3 = GameObject.FindGameObjectWithTag("MyWorldVectorCenter").transform.position;
 		gyroUnitYVec3 = centerVec3 - topVec3;
 		
-		var backVec3 : UnityEngine.Vector3 = gameObject.FindGameObjectWithTag("MyWorldVectorZ").transform.position;
+		var backVec3 : UnityEngine.Vector3 = GameObject.FindGameObjectWithTag("MyWorldVectorZ").transform.position;
 		gyroUnitZVec3 = centerVec3 - backVec3;
 		
-		var sideVec3 : UnityEngine.Vector3 = gameObject.FindGameObjectWithTag("MyWorldVectorX").transform.position;
-		gyroUnitYVec3 = centerVec3 - sideVec3;
+		var sideVec3 : UnityEngine.Vector3 = GameObject.FindGameObjectWithTag("MyWorldVectorX").transform.position;
+		gyroUnitXVec3 = centerVec3 - sideVec3;
+		
+		
+		
+		//var outputTextGyroGO : UnityEngine.GameObject = GameObject.Find("Teldstfield");
+	//var outputGyroTextC : UnityEngine.TextMesh = gameObject.FindObjectOfType(UnityEngine.TextMesh); 
+	//if(!outputTextGyroGO) {return;}
+	//	outputGyroTextC.text = gyroUnitYVec3.ToString();
+		
 		
 		var a : String = gyroUnitYVec3.ToString();
 		gameObject.GetComponentInChildren(TextMesh).text = a;
+		
+		
+		/*var outputTextGO : UnityEngine.GameObject = gameObject.FindGameObjectWithTag("OutputOnScreen");
+		var outputTextC : UnityEngine.TextMesh = outputTextGO.GetComponent(UnityEngine.TextMesh); 
+		if(!outputTextC) {return;}
+			outputTextC.text = gyroUnitZVec3.ToString();
+		*/
+		
 	}
 }
 
@@ -66,8 +94,17 @@ function passCollitionData (ownPos : UnityEngine.Vector3,otherPos : UnityEngine.
 	var YAngle : float = Vector3.Angle(gyroUnitYVec3,differenceVector);
 	var ZAngle : float = Vector3.Angle(gyroUnitZVec3,differenceVector);
 	
+	
+	var outputTextGO : UnityEngine.GameObject = gameObject.FindGameObjectWithTag("OutputOnScreen");
+	var outputTextC : UnityEngine.TextMesh = outputTextGO.GetComponent(UnityEngine.TextMesh); 
+	if(!outputTextC) {return;}
+	
+	outputTextC.text = gyroUnitXVec3.ToString("R");
+	
+	
 	if(YAngle < 60) 
 	{
+	outputTextC.text = "TOP";
 		//top or bottom collition?
 	} 
 	else if(YAngle < 120){
@@ -75,23 +112,32 @@ function passCollitionData (ownPos : UnityEngine.Vector3,otherPos : UnityEngine.
 		
 		if(XAngle < 60)
 		{
+			outputTextC.text = "RIGHT";
 			//right or left side collition?
 		}
-		else if (XAngle < 120)
+		else if (XAngle < 120){
+		//front or back
 			if(ZAngle < 90) {
+			outputTextC.text = "FRONT";
 				//front or back collition?
 			} 
 			else
 			{
+			outputTextC.text =  "BACK";
 				//front or back collition? 
 			}
-		else
+		} else
 		{
+		outputTextC.text = "LEFT";
 			//right or left side collition?
 		}
 	} else {
+	outputTextC.text = "Bottom";
 		//top or bottom collition?
 	}
+	
+	
+	
 	
  } 
 
