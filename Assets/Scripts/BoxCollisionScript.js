021abe7e1b83d6c9233020b435fac94a0415a6ca
Jakob Sand Svarstad <watashiwaore@gmail.com>
2014-02-13 11:54:18 +0100

    redid the chainconnection thing and i even commented and cleaned up in the code

--- Assets/Scripts/BoxCollisionScript.js
+++ Assets/Scripts/BoxCollisionScript.js
@@ -14,23 +14,33 @@ function Update () {
 function OnTriggerEnter (other : Collider) {
 	if(other.tag == "Player") {
 		renderer.material.color = Color.blue;
+	
+	//var side : int = MyWorldCenterC.passCollitionData(this.gameObject.transform.position,other.gameObject.transform.position);
+	//myWorldState.SetData(MyIdNumber,other.gameObject.GetComponent(BoxCollisionScript).MyIdNumber, side);
 	}
-	var side : int = MyWorldCenterC.passCollitionData(this.gameObject.transform.position,other.gameObject.transform.position);
-	myWorldState.SetData(MyIdNumber,other.gameObject.GetComponent(BoxCollisionScript).MyIdNumber, side);
 }
 
 function OnTriggerStay (other : Collider) {
 	if(other.tag == "Player") {
 		renderer.material.color = Color.green;
-	}
-	var side : int = MyWorldCenterC.passCollitionData(this.gameObject.transform.position,other.gameObject.transform.position);
-	myWorldState.SetData(MyIdNumber,other.gameObject.GetComponent(BoxCollisionScript).MyIdNumber, side);
+	
+	//var side : int = MyWorldCenterC.passCollitionData(this.gameObject.transform.position,other.gameObject.transform.position);
+	if (!MyWorldCenterC.collisionIsVertical(this.gameObject.transform.position,other.gameObject.transform.position)){
+		var side : int;
+		if(gameObject.transform.position.x < other.gameObject.transform.position.x) {
+			side = 5;
+		}
+		else {
+			side = 4;
+		}
+		myWorldState.SetData(MyIdNumber,other.gameObject.GetComponent(BoxCollisionScript).MyIdNumber, side);
 
+		}
 	}
-
+}
 function OnTriggerExit (other : Collider) {
 	if(other.tag == "Player"){
 		renderer.material.color = Color.red;
 	}
-	myWorldState.RemoveConnection(MyIdNumber , other.gameObject.GetComponent(BoxCollisionScript).MyIdNumber);
-	}
+}
+	
