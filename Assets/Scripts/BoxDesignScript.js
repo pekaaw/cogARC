d6c00b326a586cd0bccb6bcf81a722e89f6f4413
PK <pekaaw@gmail.com>
2014-03-04 18:22:32 +0100

    Cleaned up BoxDesignScript and tried to put on a proper material to get readable text.

--- Assets/Scripts/BoxDesignScript.js
+++ Assets/Scripts/BoxDesignScript.js
@@ -11,86 +11,33 @@ class BoxDesignScript extends MonoBehaviour {
 	//public var cubeText2 : TextMesh = null;
 
 	function Start() {
+		// Create GameObject to put text on. Bind it to box.
 		cubeHandle = new GameObject();
 		cubeHandle.name = "GameObject from BoxDesignScript";
 		cubeHandle.transform.parent = gameObject.transform.parent;
 		cubeHandle.transform.Rotate( 90, 0, 0 );
-		//cubeHandle.transform.Translate( 0, 0.01, 0 );
 		cubeHandle.transform.position = Vector3(0.0, 0.01, 0.0 );
 		
+		// Add a MeshRenderer with textshader
 		textRenderer = cubeHandle.AddComponent(MeshRenderer);
-		textRenderer.material = new Material( Shader.Find("GUI/Text Shader"));
+		textRenderer.material.shader = Shader.Find("GUI/Text Shader");
+		textRenderer.material = Resources.Load("Assets/Gudea-Regular.ttf");
 		
+		// Add a TextMesh with text
 		cubeText = cubeHandle.AddComponent(TextMesh);
 		cubeText.anchor = TextAnchor.MiddleCenter;
 		cubeText.text = "Heiaa!";
 		
+		
+		// Calculate new scale for text
 		var renderingSize : Vector3;
 		var newScale : float;
-		
 		renderingSize = textRenderer.bounds.size;
 		newScale = 1 / renderingSize.x;
 		
+		// Change scale of text so that it will fit onto the cube
 		cubeText.transform.localScale = Vector2(newScale, newScale); //textSize; // Vector2(1, 0);
 
-		
-//		var textSize : Vector3;
-//		var textSizeFactor : float;
-//		var textSizeNewY : float;
-//		var textSizeNew : Vector2;
-//		
-//		var cubeSize : Vector3;
-//		
-//		textSize = textRenderer.bounds.size;
-//		
-//		textSizeFactor = 1 / textSize.x;
-//		textSizeNewY = textSizeFactor;
-//		
-//		textSizeNew = Vector2( textSizeFactor, textSizeNewY );
-//		
-//		//var renderBounds : Bounds;
-//		//renderBounds = textRenderer.bounds;
-//		
-//		Debug.Log("textSizeFactor: " + textSizeFactor );
-//		Debug.Log("textSize.y = " + textSize.y );
-//		
-//		cubeSize = gameObject.transform.lossyScale;
-//		Debug.Log("cubeSize> X: " + cubeSize.x + ", Y: " + cubeSize.y + ", Z: " + cubeSize.z);
-		
-//		textSize = cubeText.transform.localScale;
-//
-//		textSizeFactor = 1 / textSize.x;
-//		textSizeY = textSize.y * textSizeFactor;
-//		
-//		textSize = Vector2( 1, textSizeNewY );
-//		
-//		
-//		cubeText.transform.localScale = textSizeNew; //textSize; // Vector2(1, 0);
-		
-		//cubeHandle = gameObject;
-		
-		
-		//Instantiate(cubeHandle, gameObject.transform.position, Quaternion.identity);
-		
-		//cubeHandle.transform.parent.transform = gameObject.transform;
-		Debug.Log("KNYTT TIL " + cubeHandle.transform.parent);
-		
-		//cubeText = cubeHandle.AddComponent(TextMesh);
-		//Debug.Log("er attached to " + gameObject.name);
-		//textRenderer = gameObject.AddComponent(MeshRenderer);
-		//cubeHandle = gameObject.AddComponent(GameObject);
-		
-		
-		//cubeText2 = new TextMesh(); //GameObject(TextMesh);
-		//cubeText2.text = "Heia!";
-		//Instantiate(cubeText2);
-		//Instantiate(cubeText);
-		//Instantiate(cubeRenderer);
-		
-		//cubeText = cubeText2.AddComponent(TextMesh);
-		
-		// cubeText2.AddComponent(cubeText);
-		
 		//cubeText.text = "Jakob sin!";
 	}
 
