650b9ccacd0520b1f886105cab10ad1b8eb9f23b
PK <pekaaw@gmail.com>
2014-03-05 16:24:42 +0100

    Refactored BoxDesignScript. Will now initiate stuff on Awake and set design by running setDesign. Removed two constructors.

--- Assets/Scripts/BoxDesignScript.js
+++ Assets/Scripts/BoxDesignScript.js
@@ -16,50 +16,47 @@ class BoxDesignScript extends MonoBehaviour {
 		fontName = "Gudea-Bold";
 	}
 	
-	function BoxDesignScript( boxDesign : BoxDesign ) {
-		BoxDesignScript();
-		design = boxDesign;
-	}
-	
 	function BoxDesignScript( font : String ) {
 		BoxDesignScript();
 		fontName = font;
 	}
 	
-	function BoxDesignScript( boxDesign : BoxDesign, font : String ) {
-		design = boxDesign;
-		fontName = font;
-	}
-	
 	public function setDesign( boxDesign : BoxDesign ) {
-		// TODO: change TextMesh according to boxDesign
+	
+		// store design
+		design = boxDesign;
+		
+		// Set color on box
+		gameObject.renderer.material.color = design.BoxColor;
+		
+		// Set text on box (with color)
+		cubeText.text = design.BoxText;
+		cubeText.color = design.TextColor;
+		
+		// Create material from texture
+		var boxMaterial : Material = new Material(Shader.Find("Transparent/Diffuse"));
+		boxMaterial.SetTexture( "_MainTex", design.BoxImage );
+		
+		// Add Material (we cannot manipulate renderer.materials directly)
+		var materials : Array = gameObject.renderer.materials;
+		materials.Add( Resources.Load("BoxPair0", UnityEngine.Material ) );
+		//materials.Add( boxMaterial );
+		gameObject.renderer.materials = materials;
+
+		setLocalScale();
 	}
 
-	public function setupBoxDesign() {
-		Awake();
-	}
-	
 	function Awake() {
-		cubeHandle = new GameObject();
-	}
-	
-	function Start() {
-		// TODO: Setup for TextMesh
-		
 		// Create GameObject to put text on. Bind it to box.
 		cubeHandle = new GameObject();
-		cubeHandle.name = "GameObject from BoxDesignScript";
+		cubeHandle.name = "TextMeshToBox";
 		cubeHandle.transform.parent = gameObject.transform.parent;
 		cubeHandle.transform.rotation = Quaternion.identity;
-		cubeHandle.transform.Rotate( Vector3( 90, 0, 0) );
-		//cubeHandle.transform.rotation = Quaternion( 90, 0, 0, 0 ); //  Rotate( 90, 0, 0 );
-		//cubeHandle.transform.localRotation = Quaternion( 90, 0, 0, 0 );
+		cubeHandle.transform.Rotate( Vector3( 90, 0, 0 ) );
 		cubeHandle.transform.localPosition = Vector3( 0.0, 0.01, 0.0 );
-		//cubeHandle.transform.position = Vector3( 0.0, 0.01, 0.0 );
 		
-		// Add a MeshRenderer with textshader
+		// Add a MeshRenderer with font-material
 		textRenderer = cubeHandle.AddComponent(MeshRenderer);
-		//textRenderer.material.shader = Shader.Find("GUI/Text Shader");
 		textRenderer.material = Resources.Load(fontName, Material);
 		
 		// Add a TextMesh with text
@@ -67,19 +64,31 @@ class BoxDesignScript extends MonoBehaviour {
 		cubeText.font = Resources.Load(fontName, Font);
 		cubeText.color = design.TextColor;
 		cubeText.anchor = TextAnchor.MiddleCenter;
-		cubeText.text = "Heiaa!";
+		cubeText.text = design.BoxText;
+	}
+	
+	function Start() {
+
+		{ // TEST
+			var bDesign : BoxDesign;
+			
+			bDesign = new BoxDesign();
+
+			setDesign( bDesign );
+		} // TEST ended
+	}
+	
+	private function setLocalScale() {
 		
-		
-		// Calculate new scale for text
 		var renderingSize : Vector3;
 		var newScale : float;
+		
+		// Get size and calculate new scaling to get text to fit
 		renderingSize = textRenderer.bounds.size;
 		newScale = 1 / renderingSize.x;
 		
 		// Change scale of text so that it will fit onto the cube
-		cubeText.transform.localScale = Vector2(newScale, newScale); //textSize; // Vector2(1, 0);
-
-		//cubeText.text = "Jakob sin!";
+		cubeText.transform.localScale = Vector2( newScale, newScale );
 	}
 
 }
