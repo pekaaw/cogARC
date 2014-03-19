dd75e72f12d5f18895d9cd7e95c5a406464b3589
Daniel Granerud <denorolyzer@gmail.com>
2014-03-19 11:09:07 +0100

    One script to add them all, and in darkness hide the work.

---
+++ Assets/Editor/NewGameCreatorInspector.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+@CustomEditor (NewGameCreator)
+class NewGameCreatorInspector extends Editor{
+	private var myself : NewGameCreator;
+	
+	function OnEnable () {
+		myself = target as NewGameCreator;
+	}
+	function OnInspectorGUI () {
+		if(GUILayout.Button("Add all needed scripts!")) {
+			var obj = new GameObject("Empty");
+			obj.AddComponent(LevelCreator);
+			obj.AddComponent(Rule);
+			obj.AddComponent(WorldState);
+			obj.AddComponent(PauseScreenScript);
+			obj.AddComponent(TimerAndScore);
+			obj.AddComponent(LoadingScreen);
+			obj.AddComponent(ScoreScreen);
+			obj.name = "Level";
+			
+			var sel = Selection.activeGameObject;
+			DestroyImmediate(sel);
+		}
+	}
+}
