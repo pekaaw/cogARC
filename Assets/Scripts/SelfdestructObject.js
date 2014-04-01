69d60aeb1c12f93238e749a414b88eca0c023596
Daniel Granerud <denorolyzer@gmail.com>
2014-04-01 15:21:02 +0200

    Deleted the prefab that had both the wrong and the right marker in it, also deleted the assisiated script that was used for testing it. Wrong mark and Right mark now are two different prefabs able to be instanceiated and they have a script that is ready for testing. The script is only supposed to delete the object after a set time.

---
+++ Assets/Scripts/SelfdestructObject.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+public var longlivety : float = 5.0;
+function Start () {
+	
+}
+
+function Update () {
+	longlivety -= Time.deltaTime;
+	
+	if(longlivety > 0){
+		//show thing
+	}
+	else{
+		Destroy(gameObject);
+	}
+}
